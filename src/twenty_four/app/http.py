7b66e05a95ce4b50afc98ca62cb0a0a5f28d0ec8
quinn <quinn@quinnweber.com>
2020-07-07 20:12:08 -0700

    move some more stuff

--- src/twenty_four/app/http.py
+++ src/twenty_four/app/http.py
@@ -8,6 +8,8 @@ from raven import Client  # type: ignore
 from raven.contrib.flask import Sentry  # type: ignore
 from raven.transport.requests import RequestsHTTPTransport  # type: ignore
 
+from twenty_four.lib import get_solutions
+
 
 app = Flask(__name__)
 sentry = Sentry(app, client=Client(transport=RequestsHTTPTransport,),)
@@ -20,5 +22,23 @@ def status() -> Response:
 
     response = jsonify({"text": "ok"})
     response.headers.add("Access-Control-Allow-Origin", "*")
+
+    return typing.cast(Response, response)
+
+
+@app.route('/api/v0/solutions', methods=['GET'])
+def index() -> Response:
+    numbers = [
+        int(i)
+        for i in request.args['values'].split(',')
+    ]
+
+    if len(numbers) != 4:
+        return 'must contain 4 comma-separated values', 500
+
+    solutions = {'values': get_solutions(numbers)}
+
+    response = jsonify(solutions)
+    response.headers.add('Access-Control-Allow-Origin', '*')
 
     return typing.cast(Response, response)
